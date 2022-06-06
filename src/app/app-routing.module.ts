5b0f93e4c82b1aab0c415e6583c125c2863e99c7
E'Gonzalez <ingenieria12@telemetrik.com.co>
2022-06-06 10:17:48 -0500

    fixed bug content dont sohwed child

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -31,7 +31,7 @@ const routes:Routes = [
           {
             path: '',
             pathMatch: 'prefix',
-            redirectTo: 'gateways-status'
+            redirectTo: 'home'
           },
           {
             path: 'home',
